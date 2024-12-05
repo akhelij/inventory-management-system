4c8ae0731fc2bde139f8451f21037f710a8ac82c
Mohamed Akhelij <mohamed.akhelij@inventiv-it.fr>
2024-12-06 00:28:32 +0100

    :hammer: create the requests back

---
+++ app/Http/Requests/Customer/StoreCustomerRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'photo' => 'image|file|max:1024',
+            'name' => 'required|string|max:50',
+            'email' => 'nullable|email|max:50',
+            'phone' => 'nullable|string|max:25',
+            'city' => 'nullable|string|max:25',
+            'limit' => 'integer',
+            'account_holder' => 'max:50',
+            'account_number' => 'max:25',
+            'bank_name' => 'max:25',
+            'address' => 'nullable|string|max:100',
+        ];
+    }
+}
