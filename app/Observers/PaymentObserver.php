cbfdbd9005978b1bdbd40d4693a1f66d9f4032be
Mohamed Akhelij <mohamed.akhelij@inventiv-it.fr>
2024-12-11 23:20:16 +0100

    :hammer: fix incrementing paid column

--- app/Observers/PaymentObserver.php
+++ app/Observers/PaymentObserver.php
@@ -15,7 +15,7 @@ class PaymentObserver
     {
         if ($payment->wasChanged('cashed_in') && $payment->cashed_in == 1) {
             $customer = $payment->customer;
-            $orders = $customer->orders()->where('due', ">", 0)->orderBy('created_at', 'desc')->get();
+            $orders = $customer->orders()->where('due', ">", 0)->get();
             $paymentAmount = $payment->amount;
             foreach ($orders as $order) {
                 if ($paymentAmount > 0) {
@@ -24,7 +24,7 @@ class PaymentObserver
                         $paymentAmount -= $due;
                         $order->update(['pay' => $order->due, 'due' => 0, 'order_status' => 2]);
                     } else {
-                        $order->update(['pay' => $paymentAmount, 'due' => $due - $paymentAmount]);
+                        $order->update(['pay' => $order->pay + $paymentAmount, 'due' => $due - $paymentAmount]);
                         $paymentAmount = 0;
                     }
                 }
