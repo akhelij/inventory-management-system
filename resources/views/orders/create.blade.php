3ad752298e73fed3da70cde8133bbdc2817542ad
mohamed akhelij <m.akhelij@gmail.com>
2025-04-25 01:26:15 +0100

    display gift btn only for admin

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -100,6 +100,7 @@
                                                             </div>
                                                         </template>
                                                     </button>
+                                                    @if(auth()->user()->hasRole('admin'))
                                                     <button
                                                         @click="addFreeItem(product)"
                                                         :disabled="isAddingToCart"
@@ -114,6 +115,7 @@
                                                             <path d="M7.5 8a2.5 2.5 0 1 0 0 -5a4.8 8 0 0 1 4.5 5a4.8 8 0 0 1 4.5 -5a2.5 2.5 0 1 0 0 5" />
                                                         </svg>
                                                     </button>
+                                                    @endif
                                                 </div>
                                             </td>
                                         </tr>
